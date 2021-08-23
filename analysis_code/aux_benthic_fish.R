33e8630f6886d290de0b2b59394b37e551f44854
Cher Chow <fycc1@st-andrews.ac.uk>
2021-08-23 14:38:37 +0100

    Add summary table code

--- analysis_code/aux_benthic_fish.R
+++ analysis_code/aux_benthic_fish.R
@@ -10,6 +10,7 @@ require(ggbeeswarm)
 
 data.benthic <- read_excel('../data/Palfrey_Benthic_2014-10.xlsx', col_names=T)
 data.fish <- read_excel('../data/Palfrey_Herbivore_2014-10.xlsx', col_names=T, skip=6)
+data.fishB <- read_excel('../data/Palfrey_Herbivore_2014-10.xlsx', col_names=T, skip=6, sheet=4)
 
 
 # Benthic relative cover --------------------------------------------------
@@ -24,6 +25,10 @@ data.benthic$Benthos <- str_replace_all(data.benthic$Benthos, 'Pocillopora|Acrop
 data.benthic <- data.benthic %>% group_by(TRANSECT, Benthos) %>% summarise(pointCount=sum(pointCount))
 data.benthic$cover <- data.benthic$pointCount/81
 
+# summary table by benthos type
+data.benthic %>% filter(TRANSECT <= 8) %>% # only keep first 8 transects so that this lines up with fish conspecific transects
+  group_by(Benthos) %>% summarise(mean=mean(cover), SD=sd(cover))
+
 ggplot(data.benthic %>% filter(TRANSECT <= 8), aes(y=Benthos, x=cover)) +
   geom_boxplot(width=0.4) +
   geom_jitter(size = 1.5, color='grey50', shape = 21, height=0.3) +
@@ -36,10 +41,24 @@ ggsave('../figures/FigS2_benthos.eps', device='eps', width = 100, height = 40, u
 
 # just conspecifics for our focal species
 data.fish <- data.fish %>% filter(str_detect(Species, 'rivulatus|frenatus|doliatus|vulpinus|sordidus|striatus|scopas|unicornis|nigricaudus'))
+data.fishB <- data.fishB %>% filter(str_detect(Species, 'rivulatus|frenatus|doliatus|vulpinus|sordidus|striatus|scopas|unicornis|nigricaudus'))
 data.fish$Species <- c('Ch. spilurus', 'Sc. frenatus', 'Sc. rivulatus', 'A. nigricauda', 'C. striatus', 'N. unicornis', 'Z. scopas', 'S. vulpinus', 'S. doliatus')
+data.fishB$Species <- c('Ch. spilurus', 'Sc. frenatus', 'Sc. rivulatus', 'A. nigricauda', 'C. striatus', 'N. unicornis', 'Z. scopas', 'S. vulpinus', 'S. doliatus')
+
 colnames(data.fish)[4:11] <- paste0('T', 1:8) # rename transect replicates
+colnames(data.fishB)[4:11] <- paste0('T', 1:8)
 data.fish <- data.fish %>% select(!`Functional Group`)
+data.fishB <- data.fishB %>% select(!`Functional Group`)
+data.fishB <- data.fishB %>% mutate(across(where(is.character) & !c(Species,Genus), .fns=as.numeric)) # fix column types
 data.fish <- pivot_longer(data.fish, cols=starts_with('T'), names_to="Transect", values_to="Count") %>% filter(Count > 0)
+data.fishB <- pivot_longer(data.fishB, cols=starts_with('T'), names_to="Transect", values_to="Biomass") %>% filter(Biomass > 0)
+
+data.fish <- full_join(data.fish, data.fishB, by=c("Species", 'Transect')) %>% select(!starts_with('Genus'))
+
+data.fish %>% group_by(Species) %>% 
+  summarise(totalC=sum(Count), totalB=sum(Biomass), 
+            meanC=mean(Count), sdC=sd(Count), 
+            meanB=mean(Biomass), sdB=sd(Biomass)) %>% View()
 
 ggplot(data.fish, aes(y=Species, x=Count)) +
   geom_boxplot(width=0.3) +
@@ -48,3 +67,12 @@ ggplot(data.fish, aes(y=Species, x=Count)) +
   theme(axis.text.y = element_text(face='italic')) +
   labs(y = 'Herbivore species', x = 'Count')
 ggsave('../figures/FigS3_conspecifics.eps', device='eps', width=100, height=100, units='mm')
+
+ggplot(data.fish, aes(y=Species, x=Biomass)) +
+  geom_boxplot(width=0.3) +
+  geom_jitter(size = 1.5, color='grey50', shape = 21, height=0.15) +
+  theme_classic(base_size = 12) + 
+  theme(axis.text.y = element_text(face='italic')) +
+  labs(y = NULL, x = 'Biomass (g)') +
+  scale_x_continuous(trans=scales::pseudo_log_trans(base=exp(1)), breaks = c(50,100,500,1000,5000,10000))
+ggsave('../figures/FigS3_conspecificB.eps', device='eps', width=100, height=100, units='mm')
